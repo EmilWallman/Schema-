16f1307de6c65222e149ea7aa616135ea1681e9a
EmilWallman <emil.wallman@elev.ga.ntig.se>
2022-12-09 14:04:26 +0100

    lite inputs

--- schema/Program.cs
+++ schema/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace schema
 {
@@ -9,38 +11,86 @@ namespace schema
 
             //Input for all values
 
+
             //Different subjects
-            string subject = "";
-            while(subject != "done" && subject != "Done" && subject != "DONE")
+
+            List<string> subjects = new List<string>();
+            string subjectInput = "";
+            while(subjectInput != "done" && subjectInput != "Done" && subjectInput != "DONE")
             {
-                Console.WriteLine("Add the different subjects, Write DONE when all have been added");
-                subject = Console.ReadLine();
-                //Add subject to a list
+                Console.WriteLine("Add the different subjects, Write DONE when all have been added, if you wan't to remove a subject write REMOVE and follow the stepps");
+                subjectInput = Console.ReadLine();
+
+                //Adds the subject to a subject list
+                subjects.Add(subjectInput);
+
+                //Add a way to remove subjects
+                Console.Clear();
             }
 
-            //Teachers and their subjects
-            string teacher = "";
-            while (teacher != "done" && teacher != "Done" && teacher != "DONE")
+
+            //Adding the teachers to the list
+
+            List<string> teachers= new List<string>();
+            string teacherInput = "";
+            while (teacherInput != "done" && teacherInput != "Done" && teacherInput != "DONE")
             {
-                Console.WriteLine("Add the different subjects, Write DONE when all have been added");
-                teacher = Console.ReadLine();
+                Console.WriteLine("Add the different subjects, Write DONE when all have been added, if you wan't to remove a techers write REMOVE and follow the stepps");
+                teacherInput = Console.ReadLine();
+
                 //Add teacher to a list
+                teachers.Add(teacherInput);
+
+                //Add a way to remove teachers
+                Console.Clear();
             }
 
-            for (int i = 0; i < /*either techer list or the subject list*/; i++)
+
+            //Making a path between teacher list and subject list 
+
+            List<int> teacher_subject_crossover = new List<int>();
+            for (int i = 0; i < teachers.Count; i++)
             {
-                Console.WriteLine("What subject is: "/*the first techer*/ + "The tutor for? Chose in the list below" );
+                Console.WriteLine("What subject is: " + teachers[i] + "The tutor for? Chose in the list below" );
+
                 //Print list of all the subjects
 
+                for (int j = 0; j < subjects.Count; j++)
+                {
+                    Console.WriteLine($"({j + 1})  " + subjects[j]);
+                }
 
+                int subject_Teacher = Convert.ToInt32(Console.ReadLine());
+
+                teacher_subject_crossover.Add(subject_Teacher - 1);
+
+                Console.Clear();
             }
-            
 
-            Console.WriteLine("What subject 1 - **");
 
-            
+            //Different classroom
 
-            //Different study halls
+            List<string> classrooms = new List<string>();
+            string classroomInput = "";
+            while (classroomInput != "done" && classroomInput != "Done" && classroomInput != "DONE")
+            {
+                Console.WriteLine("Add the different classrooms with numbers, Write DONE when all have been added, if you wan't to remove a classroom write REMOVE and follow the stepps");
+                classroomInput = Console.ReadLine();
+
+                //Add teacher to a list
+                classrooms.Add(classroomInput);
+
+                //Add a way to remove teachers
+                Console.Clear();
+            }
+
+
+            //Create the different classes
+            List<string>
+
+            Console.WriteLine("Input the class name");
+
+
 
 
             //*
@@ -56,8 +106,8 @@ namespace schema
 
             //Press GO
 
-            
-            
+
+
         }
     }
 }
